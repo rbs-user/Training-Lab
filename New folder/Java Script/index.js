b73397abb2c966fe3bef3c54cc7f887166a95045
Roshan Sharma <roshansharma5795@gmail.com>
2024-08-13 22:01:01 +0530

    Fitness Website Upload

---
+++ New folder/Java Script/index.js
@@ -0,0 +1,84 @@
+// console.log("Hello,World!");
+// var ltr const
+// var firstvar = 20;
+// datatypes----Primitive Types , Reference type data type
+// Primitive type: string , number , boolean , undefined , symbol , bigInt
+// Reference Type: Array , Object , Date , Null
+
+// var a=60;
+// console.log(a);
+
+// var a = {
+//     firstname : "Roshan",
+//     lastname : "Sharma"
+// }
+// console.log(a);
+
+// var a = null;
+// console.log(a);
+
+// var a = 90;
+// var b = 40;
+// console.log( a+b);
+
+// var b = 2000;
+// var b = 1000;
+// esy to manupulate (rechange value)(re-assign value)
+
+// let c = 3000;
+// let c = 2000;
+ // cannot re-declare
+
+// const d = 4000;
+// const d = 3000;
+// cannot re-declare and re-define
+
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// Arithematic operator
+
+let a = 10;
+let b = 20;
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+let div = a/b;
+let exp = a**b;
+let rem = a%b;
+console.log(sum, sub, mul, div, exp, rem);
+
+// Assignment Operator
+let x = 10;
+x += 20; // add assignment
+x -= 20; // sub asignment
+x *= 20; // mul assignment
+x /= 20; // div assignment
+x **= 20; // exp assignmentx
+x %= 20; // rem assignment
+// console.log(x);
+
+// Compairision operator
+
+let num1 = 10;
+let num2 = "10";
+// console.log(num1 === num2); // type coresion
+console.log(num1 !== num2); // type coreesion
+
+// console.log(5+'5');
+
+let firstname = "Roshan";
+let lastname = "Sharma";
+console.log(firstname+" "+lastname);
+
+//  Logical Operator
+
+let isLoggedIn = 10;
+let isAdmin = 20;
+console.log(isLoggedIn && isAdmin);
+console.log(isLoggedIn || isAdmin);
+
+
+
+
